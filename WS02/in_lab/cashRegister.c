e32010039ba40aa6e98afba00f1b6be63d090ec1
Donghyeon Kim <dkim167@mtrx-node04pd.dcm.senecacollege.ca>
2020-09-27 06:50:07 -0400

    Workshop 2 in-lab

--- WS02/in_lab/cashRegister.c
+++ WS02/in_lab/cashRegister.c
@@ -1,11 +1,35 @@
 //==============================================
-// Name:           Full name here
-// Student Number: #########
-// Email:          userID@myseneca.ca
-// Section:        XXX
+// Name:           Donghyeon Kim
+// Student Number: 151613197
+// Email:          dkim167@myseneca.ca
+// Section:        NSS
 // Workshop:       2 (in-lab)
 //==============================================
 
 #define _CRT_SECURE_NO_WARNINGS
 
 // Start your code below:
+
+#include<stdio.h>
+
+int main(void) {
+	double balance = 0.0;
+	int loonies = 0;
+	int quarters = 0;
+
+	// Read user input.
+	printf("Please enter the amount to be paid: $");
+	scanf("%lf", &balance);
+
+	// Count loonies.
+	loonies = (int)balance;
+	balance -= loonies;
+	printf("Loonies required: %d, balance owing $%.2lf\n", loonies, balance);
+
+	// Count quarters.
+	quarters = (int)(balance / 0.25);
+	balance -= quarters * 0.25;
+	printf("Quarters required: %d, balance owing $%.2lf\n", quarters, balance);
+
+	return 0;
+}
